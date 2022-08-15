49212529a3d79143cfd6505edf603b2d7fb451f1
ipuly <fafagitu@gmail.com>
2022-08-15 16:48:14 +0700

    Second commit

---
+++ kemampuan-dasar/kemampuan-dasar-1/latihan/powershellscripts/FirstISEScript.ps1
@@ -0,0 +1,3 @@
+Get-WmiObject -Class Win32_OperatingSystem –ComputerName localhost |
+
+Select-Object -Property CSName,LastBootUpTime
---
+++ kemampuan-dasar/kemampuan-dasar-1/latihan/powershellscripts/MyScript.ps1
@@ -0,0 +1,9 @@
+﻿@ECHO OFF
+PowerShell.exe -Command "& '%~dpn0.ps1'"
+PAUSE
+
+Write-Output 'Custom PowerShell profile in effect!'
+
+Add-Content -Path 'E:\Coding-PGT\kemampuan-dasar-1\latihan\powershellscripts\MyScript.ps1' -Value "[ZoneTransfer]`nZoneId=3" -Stream 'Zone.Identifier'
+
+Clear-Content -Path 'E:\Coding-PGT\kemampuan-dasar-1\latihan\powershellscripts\MyScript.ps1' -Stream 'Zone.Identifier'
